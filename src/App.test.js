d9740e9970fc68caffb0800c47f04c873ee9c5a8
Cameron Hodges <chodges98@gmail.com>
2024-07-31 11:17:29 -0700

    file connections updates

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Business from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Business />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
