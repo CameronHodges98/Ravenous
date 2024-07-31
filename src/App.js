3a694bf6a7482a2e4cc250ee6821f41af07d7985
Cameron Hodges <chodges98@gmail.com>
2024-07-31 11:16:13 -0700

    Ravenous page setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
 
-function App() {
+function Business() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ravenous
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
 }
 
-export default App;
+export default Business;
